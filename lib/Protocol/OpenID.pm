ddd779e7f9d6dc121bb47256abc40b74822db551
vti <viacheslav.t@gmail.com>
2010-01-18 22:03:11 +0100

    Added general OpenID constants

--- lib/Protocol/OpenID.pm
+++ lib/Protocol/OpenID.pm
@@ -5,4 +5,19 @@ use warnings;
 
 our $VERSION = '0.000101';
 
+use constant OPENID_VERSION_1_0 => 'http://openid.net/signon/1.0';
+use constant OPENID_VERSION_1_1 => 'http://openid.net/signon/1.1';
+use constant OPENID_VERSION_2_0 => 'http://specs.openid.net/auth/2.0';
+
+use constant OPENID_IDENTIFIER_SELECT =>
+  'http://specs.openid.net/auth/2.0/identifier_select';
+
+use Exporter qw(import);
+our @EXPORT = qw/
+  OPENID_VERSION_1_0
+  OPENID_VERSION_1_1
+  OPENID_VERSION_2_0
+  OPENID_IDENTIFIER_SELECT
+  /;
+
 1;
